a386fe5fb639464756ca86a93acbaf8e2d43d212
Tonynopro <suarezperezricardo@gmail.com>
2025-06-24 22:59:40 -0600

    Actualizo APP para comprobar que el servidor escucha

--- server/app.js
+++ server/app.js
@@ -94,3 +94,7 @@ app.use((req, res, next) => {
 app.listen('0.0.0.0', () => {
   console.log("Servidor listo")
 });
+
+app.get('/prueba', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
